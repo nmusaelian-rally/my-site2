8e380dcef57aace7f2d1bb9589d2a7e6fb8bd089
A M <bugzillauser11@gmail.com>
2014-04-20 20:33:04 -0600

    Added first prototype of image chooser.

--- main.js
+++ main.js
@@ -19,21 +19,62 @@ gallery.directive('imgLoad', function() { // 'imgLoad'
     };
 });
 
+Array.prototype.chunk = function(chunkSize) {
+    var R = [];
+    for (var i=0; i<this.length; i+=chunkSize)
+        R.push(this.slice(i,i+chunkSize));
+    return R;
+}
+
 var controllers = {};
 
 controllers.ButtonsController = function($scope, $window) {
     $scope.isAboutShown = false;
+    $scope.isChooserShown = false;
+    $scope.hideChooser = function() {
+        console.log("hide");
+        $scope.isChooserShown = false;
+    }
+}
+
+controllers.ThumbnailChooserController = function($scope, $window, $http, $location, $rootScope) {
+    $scope.isLoaded = false;
+    $scope.imagesLoaded = 0;
+    $scope.imageLoaded = function() {
+        $scope.imagesLoaded++;
+        if ($scope.imagesLoaded == $scope.totalImages) {
+            $scope.isLoaded = true;
+        }
+    }
+    $http.get("/img/images.json").then(function(result) {
+        $scope.totalImages = result.data.totalImages;
+        $scope.categories = result.data.data;
+    },
+    function(error) {
+        alert(error);
+    });
 }
 
 controllers.GalleryCtrl = function($scope, $window, $http, $location) {
     $scope.page = {category: $location.path().slice(1)};
     $scope.isInfoHidden = true;
+    $scope.$on('ChangeIndex', function(newid) {
+        console.log("Change");
+        for(var i = 0; i < $scope.images.length; i++) {
+            if (images[i].id == newid) {
+                images[i].active = true;
+            }
+            else {
+                images[i].active = false;
+            }
+        }
+    });
     $http.get("/img/" + $scope.page.category + "/info.json").then(function(result) {
         var imagejson = result.data; //Information about images (titles, id's, mediums)
         $scope.images = result.data.map(function(imagedat, index) {
             console.log(imagedat.id);
             return {
-                active: (index == 0 ? true : false),
+                active: (imagedat.id == '01' ? true : false),
                 image: "/img/" + $scope.page.category + "/" + imagedat.id + ".jpg"
             };
         }); //The array of objects for the carousel element
